18ba6fcf8065f436792e4743fa21d7e2bdbf452d
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-04 20:22:29 +0300

    feat: cart count in header

--- app/src/app/components/header/header.component.ts
+++ app/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public cart: CartService) {}
+}
