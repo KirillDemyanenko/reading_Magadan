2973c73444753009106fd44586e20fba02508786
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-04 20:16:56 +0300

    feat: implement cart component

--- app/src/app/components/cart/cart.component.ts
+++ app/src/app/components/cart/cart.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
+import { TuiMarkerIconModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage, RouterLink, TuiMarkerIconModule],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {}
+export class CartComponent {
+  constructor(public cart: CartService) {}
+}
