b17cd18659138bf26629457ab7470524c1306c07
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-04 17:10:36 +0300

    feat: add books types

---
+++ app/src/app/types.ts
@@ -0,0 +1,14 @@
+export type BooksDTO = {
+  error: string;
+  total: string;
+  books: Book[];
+};
+
+export type Book = {
+  title: string;
+  subtitle: string;
+  isbn13: string;
+  price: string;
+  image: string;
+  url: string;
+};
