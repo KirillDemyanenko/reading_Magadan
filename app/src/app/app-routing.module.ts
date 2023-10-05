c157355207bc1681c17c8bd6ee3121e7fd114686
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-05 10:03:49 +0300

    fix: routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { DetailComponent } from './components/detail/detail.component';
 import { CartComponent } from './components/cart/cart.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'store', component: MainComponent },
   { path: 'detail', component: DetailComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'cart', component: CartComponent },
   { path: '**', component: NotFoundComponent },
 ];
