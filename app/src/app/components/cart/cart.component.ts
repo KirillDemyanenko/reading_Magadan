23deea3af08858b7e5686c5dc0161e2d81d71e25
KirillDemyanenko <demyanenkokg@gmail.com>
2023-10-05 09:17:07 +0300

    feat: show message if cart is empty

--- app/src/app/components/cart/cart.component.ts
+++ app/src/app/components/cart/cart.component.ts
@@ -3,11 +3,21 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { RouterLink } from '@angular/router';
 import { TuiMarkerIconModule } from '@taiga-ui/kit';
+import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
+import { TuiBlockStatusModule } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, RouterLink, TuiMarkerIconModule],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    RouterLink,
+    TuiMarkerIconModule,
+    TuiLinkModule,
+    TuiButtonModule,
+    TuiBlockStatusModule,
+  ],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
